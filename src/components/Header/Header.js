af74424dd3952ad0d024d8d85fbbdffa845f5714
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-02-20 13:57:41 +0300

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Wrapper,
+  LogoWrapper,
+  HomeButton,
+  FollowButton,
+  SearchWrapper,
+  SearchBarWrapper,
+} from "./styles";
+const Header = () => {
+  return (
+    <Wrapper>
+      <LogoWrapper></LogoWrapper>
+      <HomeButton></HomeButton>
+      <FollowButton></FollowButton>
+      <SearchWrapper>
+      <SearchBarWrapper>
+      
+      </SearchBarWrapper>
+      </SearchWrapper>
+    </Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const LogoWrapper = styled.div``;
+
+export const HomeButton = styled.div``;
+
+export const FollowButton = styled.div``;
+export const SearchWrapper = styled.div``;
+
+export const SearchBarWrapper = styled.div``;
